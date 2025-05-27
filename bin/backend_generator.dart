2eae48219c610ab609aa6ab94382c2a9b7b5da10
danielac32 <danielaquinteroac32@gmail.com>
2025-05-27 16:35:31 -0400

    first commit

---
+++ bin/backend_generator.dart
@@ -0,0 +1,5 @@
+import 'package:backend_generator/backend_generator.dart' as backend_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${backend_generator.calculate()}!');
+}
---
+++ test/backend_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:backend_generator/backend_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
